181edb9a5c41186fd58e96d0ae0a3392a95bb3c8
Ilia <sar.ilia@seznam.cz>
2023-01-14 18:42:22 +0100

    fixed js code

--- js/script.js
+++ js/script.js
@@ -16,16 +16,19 @@ document.addEventListener('DOMContentLoaded', () => {
         menu.classList.toggle('dropdown__menu-open');
     });
 
+    function removeDropdown() {
+        select.classList.remove('dropdown__select-clicked');
+
+        caret.classList.remove('dropdown__caret-rotate');
+
+        menu.classList.remove('dropdown__menu-open');
+    }
 
     options.forEach(e => {
         e.addEventListener('click', () => {
             selected.innerHTML = e.innerHTML;
 
-            select.classList.remove('dropdown__select-clicked');
-
-            caret.classList.remove('dropdown__caret-rotate');
-
-            menu.classList.remove('dropdown__menu-open');
+            removeDropdown();
 
             options.forEach(e => {
                 e.classList.remove('active');
@@ -36,16 +39,12 @@ document.addEventListener('DOMContentLoaded', () => {
     
     //_____
 
-    const modal = document.querySelector('#modal'),
-          close = modal.querySelector('.close'),
+    const modal = document.querySelector('.modal'),
+          close = document.querySelectorAll('#close_modal'),
           start = document.querySelector('#start');
 
     start.addEventListener('click', () => {
         modal.style.display = 'block';
-    });
-
-    close.addEventListener('click', () => {
-        modal.style.display = 'none';
     });
 
     function removeForm(weightContent, repeatsContent) {
@@ -62,107 +61,101 @@ document.addEventListener('DOMContentLoaded', () => {
     const addElement = document.querySelector('#add'),
           weight = document.querySelector('#weight'),
           repeats = document.querySelector('#repeats'),
+          modals = document.querySelector('.modals'),
           modalRepeats = document.querySelector('#modalRepeats'),
-          addRepeats = document.querySelector('#addRepeats');
+          addRepeats = document.querySelector('#addRepeats'),
+          weightRepeats = document.querySelector('#weightRepeats'),
+          repeatsRepeats = document.querySelector('#repeatsRepeats');
 
+    function delElement(element) {
+        content.addEventListener('click', (e) => {
+            let del = e.target.closest(element);
+            if (e.target && e.target.matches(element)) {
+                del.parentElement.remove();
+            }
+        });
+    }
+
+    delElement('button.btn_descr');
+
+    delElement('.close');
+
+    function closeModal(modalElem, weightElem, repeatsElem) {
+        close.forEach( e => {
+            e.addEventListener('click', () => {
+                modalElem.style.display = 'none';
+                removeForm(weightElem, repeatsElem);
+                removeDropdown();
+            });
+        });
+    }
+
+    closeModal(modal, weight, repeats);
+    
     addElement.addEventListener('click', () => {
 
         const element = document.createElement('div');
               element.classList.add('content__div');
-
-        const contentElement = document.createElement('h2');
-              contentElement.classList.add('content__titel');
-              contentElement.textContent = `Exercise: ${selected.textContent}`;
-
-        const deleteElement = document.createElement('div');
-              deleteElement.classList.add('close');
-              deleteElement.innerHTML = '&times;';
-              deleteElement.setAttribute('id', 'del_elem');
-
-        const wrapper = document.createElement('div');
-              wrapper.classList.add('content__wrapper');
-        
-        const list = document.createElement('ul');
-              list.classList.add('content__list');
-        
-        const item = document.createElement('li');
-              item.classList.add('content__item');
-              item.textContent = `${+weight.value}kg x ${+repeats.value}`;
-
-        const deleteDescr = document.createElement('button');
-              deleteDescr.classList.add('btn_descr');
-              deleteDescr.textContent = 'DELETE';
-
-        const oneMore = document.createElement('button');
-              oneMore.classList.add('btn', 'btn_mini');
-              oneMore.textContent = 'one more?';
-
+              element.innerHTML = `
+                <h2 class="content__titel">Exercise: ${selected.textContent}</h2>
+                <div class="close" id="del_elem">×</div>
+                <div class="content__wrapper">
+                    <ul class="content__list">
+                        <li class="content__item">${+weight.value}kg x ${+repeats.value}
+                            <button class="btn_descr">DELETE</button>
+                        </li>
+                    </ul>
+                </div>
+                <button class="btn btn_mini">one more?</button>
+              `;
 
         if (selected.textContent !== 'Type of exercise' && weight.value !== '' && repeats.value !== '' && weight.value != 0 && repeats.value != 0) {
-            element.appendChild(contentElement);
             content.appendChild(element);
-            element.appendChild(deleteElement);
-            element.appendChild(wrapper);
-            wrapper.appendChild(list);
-            list.append(item);
-            item.appendChild(deleteDescr);
-            element.appendChild(oneMore);
 
             modal.style.display = 'none';
 
             removeForm(weight, repeats);
         }
+    });
 
-        deleteElement.addEventListener('click', () => {
-            content.removeChild(element);
-        });
+    content.addEventListener('click', (e) => {
+        if (e.target && e.target.matches('button.btn_mini')) {
+            modalRepeats.style.display = 'block';
 
-        function delDescr() {
-            document.querySelectorAll('.btn_descr').forEach((btn) => {
-                btn.addEventListener('click', () => {
-                    btn.parentElement.remove();
-                });
-            });
+            const btns = document.querySelectorAll('.btn_mini');
+            
+            if(e.target.classList.contains('btn_mini')) {
+                for (let i = 0; i < btns.length; i++) {
+                  btns[i].removeAttribute('id');
+                }
+                e.target.setAttribute('id','qwe');
+            }
         }
+    });
 
-        delDescr();
+    closeModal(modalRepeats, weightRepeats, repeatsRepeats);
 
-        function adddd() {
-            document.querySelectorAll('.btn_mini').forEach((btn) => {
-                btn.addEventListener('click', () => {
-                    modalRepeats.style.display = 'block';
-        
-                    addRepeats.addEventListener('click', () => {
-        
-                        const weightRepeats = document.querySelector('#weightRepeats'),
-                              repeatsRepeats = document.querySelector('#repeatsRepeats');
-                        
-                        const item = document.createElement('li');
-                              item.classList.add('content__item');
-                              item.textContent = `${+weightRepeats.value}kg x ${+repeatsRepeats.value}`;
-                
-                        const deleteDescr = document.createElement('button');
-                              deleteDescr.classList.add('btn_descr');
-                              deleteDescr.textContent = 'DELETE';
-            
-            
-                        if (weightRepeats.value !== '' && repeatsRepeats.value !== '' && weightRepeats.value != 0 && repeatsRepeats.value != 0) {
-                            list.appendChild(item);
-                            item.appendChild(deleteDescr);
-                
-                            modalRepeats.style.display = 'none';
-                
-                            removeForm(weightRepeats, repeatsRepeats);
-                        }
-            
-                        delDescr();
-            
-                    });
-                });
-            });
+    modals.addEventListener('click', (e) => {
+
+        if (e.target && e.target.matches('#addRepeats')) {
+
+            const btnActive = document.querySelector('#qwe').previousElementSibling.firstElementChild;
+
+            const item = document.createElement('li');
+                  item.classList.add('content__item');
+                  item.innerHTML = `
+                    ${+weightRepeats.value}kg x ${+repeatsRepeats.value}
+                    <button class="btn_descr">DELETE</button>
+                  `;
+
+            if (weightRepeats.value !== '' && repeatsRepeats.value !== '' && weightRepeats.value != 0 && repeatsRepeats.value != 0) {
+                btnActive.appendChild(item);
+    
+                modalRepeats.style.display = 'none';
+    
+                removeForm(weightRepeats, repeatsRepeats);
+            }
         }
-
-        adddd();
     });
 
     //____
